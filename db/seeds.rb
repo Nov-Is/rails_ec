5f80cc6688b6d51b1b626abe0a514cf759af0a2b
Nov-Is <jza80.na2@gmail.com>
2024-06-01 14:10:35 +0900

    商品一覧機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  Product.create!(
+    name: 'shoes',
+    description: '靴です',
+    price: 1000,
+    stock: 5,
+    image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/6c757d.jpg')),
+                                                  filename: 'test.jpg')
+  )
+end
