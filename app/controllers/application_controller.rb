f470e0e8f9722a52623525021098e25ce139aef5
Nov-Is <jza80.na2@gmail.com>
2024-06-17 23:38:55 +0900

    find_or_create_byメソッドで書き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def current_cart
-    if session[:cart_id]
-      @cart = Cart.find(session[:cart_id])
-    else
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
+    @cart = Cart.find_or_create_by(id: session[:cart_id])
+    session[:cart_id] = @cart.id
+    @cart
   end
 end
