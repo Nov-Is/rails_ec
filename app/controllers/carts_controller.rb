033eb3b7a4fbaf9023113b7639f32d142d41bc04
Nov-Is <jza80.na2@gmail.com>
2024-06-23 14:08:53 +0900

    eager_load,エラーハンドリングを追記

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,15 +4,19 @@ class CartsController < ApplicationController
   before_action :setup_cart_item!, only: %i[create]
 
   def show
-    @cart_items = current_cart.cart_items
+    @cart_items = current_cart.cart_items.eager_load(:product)
     @total = @cart_items.inject(0) { |sum, product| sum + product.sum_of_price }
   end
 
   def create
     @cart_item = current_cart.cart_items.new(product_id: params[:product_id]) if @cart_item.blank?
     @cart_item.quantity += 1
-    @cart_item.save
-    flash[:notice] = "商品「#{@cart_item.product.name}」をカートに追加しました。"
+    if @cart_item.quantity <= @cart_item.product.stock
+      @cart_item.save
+      flash[:notice] = "商品「#{@cart_item.product.name}」をカートに追加しました。"
+    else
+      flash[:notice] = "商品「#{@cart_item.product.name}」をカートに追加できません。"
+    end
     redirect_to root_path
   end
 
