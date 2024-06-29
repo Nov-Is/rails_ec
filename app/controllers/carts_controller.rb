2eda768f5b9788e8044e5e1b14ffd8bbf222a3fe
Nov-Is <jza80.na2@gmail.com>
2024-06-29 14:57:07 +0900

    詳細画面からカートに追加、カート画面から削除機能追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  before_action :setup_cart_item!, only: %i[create]
+  before_action :set_product_id, :setup_cart_item!, only: %i[create destroy]
 
   def show
     @cart_items = current_cart.cart_items.eager_load(:product)
@@ -9,8 +9,8 @@ class CartsController < ApplicationController
   end
 
   def create
-    @cart_item = current_cart.cart_items.new(product_id: params[:product_id]) if @cart_item.blank?
-    @cart_item.quantity += 1
+    @cart_item = current_cart.cart_items.new(product_id: @product_id) if @cart_item.blank?
+    @cart_item.quantity += params[:quantity].to_i
     if @cart_item.quantity <= @cart_item.product.stock
       @cart_item.save
       flash[:notice] = "商品「#{@cart_item.product.name}」をカートに追加しました。"
@@ -20,9 +20,20 @@ class CartsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @cart_item.destroy
+    flash[:notice] = "カートから商品「#{@cart_item.product.name}」を削除しました。"
+    redirect_to cart_path(@cart_item.cart_id)
+  end
+
   private
 
+  # index,showからの受取値を整形
+  def set_product_id
+    @product_id = (params[:product_id] || params[:format])
+  end
+
   def setup_cart_item!
-    @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
+    @cart_item = current_cart.cart_items.find_by(product_id: @product_id)
   end
 end
