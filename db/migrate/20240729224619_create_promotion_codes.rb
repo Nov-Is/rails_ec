f40176c6174cb4821823ae924fbdf1c6dcafb95e
Nov-Is <jza80.na2@gmail.com>
2024-07-30 07:54:15 +0900

    promotion_codesモデル、promotion_codeタスク作成

---
+++ db/migrate/20240729224619_create_promotion_codes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreatePromotionCodes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :promotion_codes do |t|
+      t.string :promotion_code, null: false
+      t.integer :discount_amount, null: false
+      t.boolean :available, null: false
+      t.references :order, optional: true, foreign_key: true, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
