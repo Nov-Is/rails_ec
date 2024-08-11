b66df03e9919a79a5ed1275511b95177224dbb69
Nov-Is <jza80.na2@gmail.com>
2024-08-11 13:49:53 +0900

    コード修正

--- app/controllers/promotion_codes_controller.rb
+++ app/controllers/promotion_codes_controller.rb
@@ -3,7 +3,7 @@
 class PromotionCodesController < ApplicationController
   def update
     promotion_code = PromotionCode.find_by(promotion_code: params[:cart][:promotion_code])
-    unless promotion_code.cart_id.nil?
+    if promotion_code.cart_id.present?
       flash[:notice] = 'このコードは使用されています。'
       redirect_to cart_path(current_cart)
       return
