66123f55aa85a1790adef3068fc7c9ca5c9163a4
Nov-Is <jza80.na2@gmail.com>
2024-08-12 12:47:10 +0900

    orders_controllerの修正、パーシャルへの切り出し

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,6 +5,8 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation(order)
     @order = order
+    @items = OrderDetail.items(@order.id)
+    @promotion_code = PromotionCode.applied(@order.id)
     mail(
       subject: 'ご購入ありがとうございます。',
       to: @order.email
